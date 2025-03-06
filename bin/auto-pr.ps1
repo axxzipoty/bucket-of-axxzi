8a70597f7308f815dbdcfb5742fe3cd12c220c10
axxzipoty <git.quiver864@passinbox.com>
2025-03-06 22:40:30 +0100

    Update auto-pr.ps1

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "axxzipoty/bucket-of-axxzi:master"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
